5251234800e890439712df75937deebc14aa76b1
msachi <marchee88@gmail.com>
2016-11-07 15:49:02 +0000

    send blog post

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var http = require('http');
 var fs = require('fs');
+var querystring = require('querystring')
 var server = http.createServer(handler);
 var message = 'I am so happy to be part of the Node Girls workshop!';
+var allTheData = '';
 
 function handler (request, response) {
   var endpoint = request.url;
   console.log(endpoint);
   var method = request.method;
   console.log(method);
-  if (endpoint == '/') {
+  if (endpoint == '/create-post') {
+    response.writeHead(302, {"Location": "/index.html"});
+    request.on('data', function (chunkOfData) {
+      allTheData += chunkOfData;
+    });
+
+    request.on('end', function () {
+      var convertedData = querystring.parse(allTheData);
+      console.log(convertedData);
+      response.end();
+    });
+  } else if (endpoint == '/') {
     response.writeHead(200, { "content-type": "text/html"} );
     fs.readFile(__dirname + '/public/index.html', function(error, file) {
       if (error) {
@@ -19,7 +32,7 @@ function handler (request, response) {
     });
   } else {
     response.writeHead(200, { "content-type": "text/" + endpoint.split('.')[1]} );
-    fs.readFile(__dirname + '/public/' + endpoint, function(error, file) {
+    fs.readFile(__dirname + '/public' + endpoint, function(error, file) {
       if (error) {
         console.log (error);
         return;
