2486ab86671e4d23503f233d49e24a6694ede896
msachi <marchee88@gmail.com>
2016-11-07 16:19:02 +0000

    Delete everything

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var myModule = require('./src/handler.js');
-
-var server = http.createServer(myModule);
-// var message = 'I am so happy to be part of the Node Girls workshop!';
-
-server.listen(3000, function() {
-  console.log("Server is listening on port 3000.  Ready to accept requests!");
-});
