b567a722ee9f0d043e514a5bdb577c6220dcc180
msachi <marchee88@gmail.com>
2016-11-07 14:47:16 +0000

    Request/response exercise

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer(handler);
 var message = 'I am so happy to be part of the Node Girls workshop!';
 
 function handler (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(message); //response body
-  response.end(); // finish response
+  var endpoint = request.url;
+  console.log(endpoint);
+  var method = request.method;
+  console.log(method);
+  if (endpoint == '/') {
+    response.writeHead(200, { "content-type": "text/html"} );
+    fs.readFile(__dirname + '/public/index.html', function(error, file) {
+      if (error) {
+        console.log (error);
+        return;
+      }
+      response.end(file);
+    });
+  }
+  // if (endpoint == '/node') {
+  //   response.writeHead(200, {"Content-Type": "text/html"});
+  //   response.write('Node!'); //response body
+  //   response.end(); // finish response
+  // } else if (endpoint == '/girls') {
+  //   response.writeHead(200, {"Content-Type": "text/html"});
+  //   response.write('Girls!'); //response body
+  //   response.end(); // finish response
+  // }
 }
 
 server.listen(3000, function() {
